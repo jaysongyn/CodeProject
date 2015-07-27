9fc9d1014a4b35b1274e81de1b28a8e33f555624
jayson <jayson.inf@gmail.com>
2015-07-27 16:12:45 -0700

    primeira parte projeto

---
+++ app/Client.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CodeProject;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected  $fillable = [
+        'name',
+        'responsable',
+        'email',
+        'phone',
+        'address',
+        'obs'
+    ];
+
+}
